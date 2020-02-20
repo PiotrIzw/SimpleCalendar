135a036f944a4d3740d048e89308356a43e83533
PiotrIzw <piotr.izworski.0@gmail.com>
2020-02-20 20:01:14 +0100

    Added comments, deleted unnecessary parts of code

--- src/CalendarTest.java
+++ src/CalendarTest.java
@@ -7,32 +7,30 @@ public class CalendarTest {
     {
 
         LocalDate date = LocalDate.now();
-        int month = date.getMonthValue();
-        int today = date.getDayOfMonth();
+        int month = date.getMonthValue(); //current month value
+        int today = date.getDayOfMonth(); //current day of month
 
-        date = date.minusDays(today - 1);
+        date = date.minusDays(today - 1); //setting date to first day of the month by subtracting current date by it's value
 
-        DayOfWeek weekday = date.getDayOfWeek();
+        DayOfWeek weekday = date.getDayOfWeek(); //current day of week
         int value = weekday.getValue();
 
         System.out.println(" Mn  Tu  We  Th  Fr  We  Su");
         for (int i = 1; i < value; i++)
-            System.out.print("    ");
+            System.out.print("    "); //printing spaces to adjust first row
 
         while (date.getMonthValue() == month) {
-            System.out.printf("%3d", date.getDayOfMonth());
+            System.out.printf("%3d", date.getDayOfMonth()); //printing day of month using 3 decimal symbols
 
             if (date.getDayOfMonth() == today)
                 System.out.print("*");
             else
                 System.out.print(" ");
 
-            date = date.plusDays(1);
+            date = date.plusDays(1); //incrementing day
 
-            if (date.getDayOfWeek().getValue() == 1)
-                System.out.println();
+            if (date.getDayOfWeek().getValue() == 1) //if we moved to next week
+                System.out.println();               // move to next time
         }
-        if (date.getDayOfWeek().getValue() != 1)
-            System.out.println();
     }
 }
